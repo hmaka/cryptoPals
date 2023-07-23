18ce7f7ffaa1129a9e17d5eed0a97994bdcd8b76
Havish <Havish747@gmail.com>
2023-07-23 19:31:13 -0400

    Rename to create helpers mod

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-pub mod create_letter_freq;
 pub mod crypto_pals_01;
 pub mod crypto_pals_02;
 pub mod crypto_pals_03;
 pub mod crypto_pals_04;
 pub mod crypto_pals_05;
+pub mod helpers;
 
 fn main() {
     let file_name = "Frankenstein.txt";
-    create_letter_freq::get_letter_freq(file_name);
+    helpers::get_letter_freq(file_name);
 }
